204f12cddb3a6bf800d39a36c0904abe89a509ea
x-iran-x <mrxmanam@gmail.com>
2026-08-20 00:42:33 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,60 @@
+const CACHE_NAME = "peykan-khan-cache-v1";
 
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json"
+];
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames
+                    .filter(function (cacheName) {
+                        return cacheName !== CACHE_NAME;
+                    })
+                    .map(function (cacheName) {
+                        return caches.delete(cacheName);
+                    })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(function (cachedResponse) {
+            return (
+                cachedResponse ||
+                fetch(event.request).then(function (networkResponse) {
+                    const responseClone = networkResponse.clone();
+
+                    caches.open(CACHE_NAME).then(function (cache) {
+                        cache.put(event.request, responseClone);
+                    });
+
+                    return networkResponse;
+                })
+            );
+        })
+    );
+});
